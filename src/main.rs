18a0555d0a4c562a576eefd33e24887aa02caf75
Alistair Keiller <akeiller@ohs.stanford.edu>
2024-09-13 19:50:08 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,14 @@ include_cpp! {
 fn main() {
     cxx::let_cxx_string!(can = "can0");
     let mut falcon =
-        ffi::ctre::phoenix::motorcontrol::can::TalonFX::new(c_int(1), &can).within_unique_ptr();
+        ffi::ctre::phoenix::motorcontrol::can::TalonFX::new(c_int(0), &can).within_unique_ptr();
     for _ in 0..50 {
         falcon.pin_mut().Set(
             ffi::ctre::phoenix::motorcontrol::TalonFXControlMode::PercentOutput,
-            0.5,
+            0.1,
         );
 
-        // ffi::ctre::phoenix::unmanaged::Unmanaged::FeedEnable(c_int(100));
+        ffi::ctre::phoenix::unmanaged::Unmanaged::FeedEnable(c_int(100));
 
         std::thread::sleep(std::time::Duration::from_millis(100));
     }
--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import time
 import phoenix6
 
+talon = phoenix6.hardware.TalonFX(0)
+talon_out = phoenix6.controls.DutyCycleOut(0.1)
 for _ in range(100):
-    talon = phoenix6.hardware.TalonFX(0)
-    talon_out = phoenix6.controls.DutyCycleOut(0.1)
-    
     talon.set_control(talon_out)
     
     phoenix6.unmanaged.feed_enable(0.100)

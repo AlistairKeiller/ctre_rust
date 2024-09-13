24dedbf7179a9ef321e63ee5516b2c31b0a43b59
Alistair Keiller <akeiller@ohs.stanford.edu>
2024-09-13 05:18:44 +0000

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import time
+import phoenix6
+
+for _ in range(100):
+    talon = phoenix6.hardware.TalonFX(1)
+    talon_out = phoenix6.controls.DutyCycleOut(talon)
+    
+    talon.set_control(talon_out)
+    
+    phoenix6.unmanaged.feed_enable(0.100)
+    
+    time.sleep(0.1)

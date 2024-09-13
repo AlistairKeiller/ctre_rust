e29acfa466d483f71a2334cbfb6f46375534d5ec
Alistair Keiller <akeiller@ohs.stanford.edu>
2024-09-13 20:53:18 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,13 @@ fn main() {
     cxx::let_cxx_string!(can = "can0");
     let mut falcon =
     ffi::ctre::phoenix::motorcontrol::can::TalonFX::new(c_int(0), &can).within_unique_ptr();
-    falcon.pin_mut().Set(
-        ffi::ctre::phoenix::motorcontrol::TalonFXControlMode::Position,
-        0.0,
-
-    );
-    ffi::ctre::phoenix::unmanaged::Unmanaged::FeedEnable(c_int(100));
     for i in 0..10000 {
+        falcon.pin_mut().Set(
+            ffi::ctre::phoenix::motorcontrol::TalonFXControlMode::PercentOutput,
+            0.1,
+    
+        );
+        ffi::ctre::phoenix::unmanaged::Unmanaged::FeedEnable(c_int(100));
 
         std::thread::sleep(std::time::Duration::from_millis(100));
     }

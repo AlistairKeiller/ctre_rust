10f6f388a76192135226dcaf8a80ebbacc2d8320
Alistair Keiller <akeiller@ohs.stanford.edu>
2024-09-13 20:47:49 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ fn main() {
     cxx::let_cxx_string!(can = "can0");
     let mut falcon =
         ffi::ctre::phoenix::motorcontrol::can::TalonFX::new(c_int(0), &can).within_unique_ptr();
-    for i in 0..100 {
+    for i in 0..10000 {
         falcon.pin_mut().Set(
             ffi::ctre::phoenix::motorcontrol::TalonFXControlMode::MotionMagic,
             i as f64*100.0,
-            
+
         );
 
         ffi::ctre::phoenix::unmanaged::Unmanaged::FeedEnable(c_int(100));

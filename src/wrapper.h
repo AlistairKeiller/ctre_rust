9c19c16b3ff14d1047276f978fa76688b4870c37
Alistair Keiller <akeiller@ohs.stanford.edu>
2024-09-17 05:55:08 +0000

    update

--- src/wrapper.h
+++ src/wrapper.h
@@ -10,9 +10,9 @@ void configure_talonfx(int device_ID)
     ctre::phoenix6::configs::TalonFXConfiguration talonfx_config{};
     ctre::phoenix6::hardware::TalonFX talonfx{device_ID};
 
-    talonfx_config.slot0.kP = 4.8;
-    talonfx_config.slot0.kI = 0;
-    talonfx_config.slot0.kD = 0.1;
+    talonfx_config.Slot0.kP = 4.8;
+    talonfx_config.Slot0.kI = 0;
+    talonfx_config.Slot0.kD = 0.1;
 
     talonfx_config.MotionMagic.MotionMagicCruiseVelocity = 1;
     talonfx_config.MotionMagic.MotionMagicAcceleration = 1;

36257cb496c46798809f63a54be31b1954b51727
Alistair Keiller <akeiller@ohs.stanford.edu>
2024-09-17 05:19:08 +0000

    test

--- src/wrapper.h
+++ src/wrapper.h
@@ -3,14 +3,12 @@
 #include <ctre/phoenix6/TalonFX.hpp>
 #include <ctre/phoenix6/controls/DutyCycleOut.hpp>
 #include <ctre/phoenix6/unmanaged/Unmanaged.hpp>
-#include <iostream>
 
 void run_talonfx(int DeviceID, double Output)
 {
     ctre::phoenix6::hardware::TalonFX talonfx(DeviceID);
     ctre::phoenix6::controls::DutyCycleOut dutyCycleOut(Output);
     talonfx.SetControl(dutyCycleOut);
-    std::cout << "TalonFX " << DeviceID << " set to " << Output << std::endl;
 }
 
 void feed_enable() {
